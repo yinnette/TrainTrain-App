9418856e328d17bf0d9e0bc413f89023f111164d
Yinnette Olivo <yinnetteolivo@gmail.com>
2016-01-29 08:26:23 -0500

    Style Changes

--- app/profile/profile.js
+++ app/profile/profile.js
@@ -24,28 +24,29 @@ angular.module('myApp.profile', ['ngRoute'])
 			'postLocation' : 'Tampa, FL', 
 			'postDate' : '12/14/15' 
 		},
-		
-		{'postImage' : '2.jpg', 'postTrophies' : '23' }, 
-		{'postImage' : '3.jpg', 'postTrophies' : '167' }, 
-		{'postImage' : '4.jpg', 'postTrophies' : '80' },
-		{'postImage' : '5.jpg', 'postTrophies' : '45' },
-		{'postImage' : '6.jpg', 'postTrophies' : '67' },
-		{'postImage' : '7.jpg', 'postTrophies' : '78' },
-		{'postImage' : '8.jpg', 'postTrophies' : '' },
-		{'postImage' : '9.jpg', 'postTrophies' : '1234' },
-		{'postImage' : '10.jpg', 'postTrophies' : '232' },
-		{'postImage' : '11.jpg', 'postTrophies' : '867' },
-		{'postImage' : '12.jpg', 'postTrophies' : '968' },
-		{'postImage' : '13.jpg', 'postTrophies' : '3984' },
-		{'postImage' : '14.jpg', 'postTrophies' : '46' },
-		{'postImage' : '15.jpg', 'postTrophies' : '34' },
-		{'postImage' : '16.jpg', 'postTrophies' : '12' }
+		{'postImage' : '2.jpg', 'postTrophies' : '23'}, 
+		{'postImage' : '3.jpg', 'postTrophies' : '167'}, 
+		{'postImage' : '4.jpg', 'postTrophies' : '80'},
+		{'postImage' : '5.jpg', 'postTrophies' : '45'},
+		{'postImage' : '6.jpg', 'postTrophies' : '67'},
+		{'postImage' : '7.jpg', 'postTrophies' : '78'},
+		{'postImage' : '8.jpg', 'postTrophies' : ''},
+		{'postImage' : '9.jpg', 'postTrophies' : '1234'},
+		{'postImage' : '10.jpg', 'postTrophies' : '232'},
+		{'postImage' : '11.jpg', 'postTrophies' : '867'},
+		{'postImage' : '12.jpg', 'postTrophies' : '968'},
+		{'postImage' : '13.jpg', 'postTrophies' : '3984'},
+		{'postImage' : '14.jpg', 'postTrophies' : '46'},
+		{'postImage' : '15.jpg', 'postTrophies' : '34'}
 	],
 
 	$scope.profile = [
 		{
-			'profileUsername' : 'yinnette', 
-			'profile' : ''
+			'userName' : 'yinnette', 
+			'userLevel' : '2', 
+			'userLocation' : 'Tampa', 
+			'userCrew' : 'TheBunch'
+
 		}
 	]
 

15c8546af87acdde95342679562f37163b554fb2
Yinnette Olivo <yinnetteolivo@gmail.com>
2016-02-01 13:06:12 -0500

    Styles

--- app/profile/profile.js
+++ app/profile/profile.js
@@ -15,45 +15,45 @@ angular.module('myApp.profile', ['ngRoute'])
 
 .controller('ProfileCtrl', [ '$scope', function($scope) {
 
+
+
+
 	$scope.posts = [
 		{ 
-			'postImage' : '1.jpg', 
-			'postTrophies' : '5', 
-			'postComments' : [ 
-					'Wow, Thats Awesome!', 
-					'Wow, Thats Cool!'
-				 ], 
-			'postTrainer' : 'RocksTraining', 
-			'postGym' : 'YouFit Gym',
-			'postLocation' : 'Tampa, FL', 
-			'postDate' : '12/14/15' 
-		},
-		{'postImage' : '2.jpg', 'postTrophies' : '23','postTrainer' : 'The_Body_Builder'}, 
-		{'postImage' : '3.jpg', 'postTrophies' : '167'}, 
-		{'postImage' : '4.jpg', 'postTrophies' : '80'},
-		{'postImage' : '5.jpg', 'postTrophies' : '45'},
-		{'postImage' : '6.jpg', 'postTrophies' : '67'},
-		{'postImage' : '7.jpg', 'postTrophies' : '78'},
-		{'postImage' : '8.jpg', 'postTrophies' : ''},
-		{'postImage' : '9.jpg', 'postTrophies' : '1234'},
-		{'postImage' : '10.jpg', 'postTrophies' : '232'},
-		{'postImage' : '11.jpg', 'postTrophies' : '867'},
-		{'postImage' : '12.jpg', 'postTrophies' : '968'},
-		{'postImage' : '13.jpg', 'postTrophies' : '3984'},
-		{'postImage' : '14.jpg', 'postTrophies' : '46'},
-		{'postImage' : '15.jpg', 'postTrophies' : '34'}
+			image : '1.jpg', 
+			trophies : '6', 
+			trainer : 'RocksTraining', 
+			gym : 'YouFit Gym',
+			location : 'Tampa, FL', 
+			date : '12/14/15',
+			comments : [ 
+				{
+					user: 'Rockhard_Abz',
+					comment : 'Wow, keep rocking on! This is what i need to start doing. ', 
+					url: ''
+				},
+				{
+					user: 'abby_workz',
+					comment : 'Love the shorts',
+					url: ''
+				}			
+			]
+		}
 	],
 
-	$scope.profile = [
-		{
-			'userName' : 'yinnette', 
-			'userLevel' : '2', 
-			'userLocation' : 'Tampa', 
-			'userCrew' : 'TheBunch'
-
+	$scope.myprofile = [
+		{ 
+			name : 'yinnette', 
+			level : '2', 
+			location : 'Tampa', 
+			crew : [ 
+				{
+					user : 'Rockhard_Abz',
+					user : 'rockerAbs10'
+				}		
+			]
 		}
-	]
-
+	],
 
 	$scope.viewOptions = "thumbnails";
 	$scope.changeView = function(){
@@ -62,6 +62,8 @@ angular.module('myApp.profile', ['ngRoute'])
 		else
 			$scope.viewOptions = "thumbnails";
 	};
+
+
 
 
 
